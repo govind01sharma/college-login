cccb59706f36b63c98afea558460d16242df5970
Govind Sharma <govind01sharma@yahoo.in>
2025-02-19 23:04:59 +0530

    Students and Upload models created in the backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const CollegeModel = require("./models/College")
+const StudentsModel = require("./models/Students")
+const UploadsModel = require("./models/Uploads")
 
 const app = express()
 app.use(express.json())
