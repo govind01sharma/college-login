ccc330e499055c8e453a5ecdad948f7f42c94ee3
Govind Sharma <govind01sharma@yahoo.in>
2025-02-24 23:29:30 +0530

    Unnecessary code removed from index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const multer = require("multer");
-const path = require("path");
-const CollegeModel = require("./models/College");
-const StudentsModel = require("./models/Students");
 
 const studentsRoutes = require("./routes/students");
 const authRoutes = require("./routes/auth");
@@ -15,18 +11,7 @@ const resumeRoutes = require("./routes/resume");
 const app = express();
 app.use(express.json());
 app.use(cors());
-const fs = require("fs");
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // Append the file extension
-    }
-});
 
-const upload = multer({ storage: storage });
 
 mongoose.connect("mongodb://127.0.0.1:27017/college")
 .then(() => console.log("MongoDB connected successfully"))
